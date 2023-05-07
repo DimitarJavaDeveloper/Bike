4593cc01ab06c0d8c433e93f49b9647da724bdde
unknown <dimitarbelcovski29@gmail.com>
2023-05-07 22:11:02 +0200

    first commit

---
+++ Bike.java
@@ -0,0 +1,12 @@
+ public class Bike {
+   Bike(){
+	System.out.println("Bike e kreiran");}
+   public void dog() {
+	   System.out.println("Grr");
+   }
+public static void main(String args[]) {
+	Bike b = new Bike();
+	b.dog();
+
+}
+}
